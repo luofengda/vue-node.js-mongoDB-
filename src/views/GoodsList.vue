b968c65c163c587c5d65be9bd9005b92792805d6
luofengda <362861693@qq.com>
2018-06-25 01:04:47 +0800

    基础库的引入
    引入axios、bulma、vuex

---
+++ src/views/GoodsList.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div>
+            <!-- <NavHeader></NavHeader> -->
+        </div>
+        <div>
+            <h1>内容</h1>
+            <div class="columns">
+                <div class="column">1</div>
+                <div class="column">2</div>
+                <div class="column">3</div>
+                <div class="column">4</div>
+                <div class="column">5</div>
+            </div>
+            <div class="columns">
+  <div class="column">
+    First column
+  </div>
+  <div class="column">
+    Second column
+  </div>
+  <div class="column">
+    Third column
+  </div>
+  <div class="column">
+    Fourth column
+  </div>
+</div>
+        </div>
+    </div>
+</template>
+<script >
+// import NavHeader from '@/components/NavHeader.vue'
+export default {
+  data() {
+    return {
+      msg: "测试显示"
+    };
+  },
+  created() {},
+  mounted() {},
+  components: {
+    //   NavHeader
+  },
+  methods: {}
+};
+</script>
+<style scoped>
+</style>
