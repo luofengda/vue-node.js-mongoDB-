4ecd7ebb0e31b4fd19f6acf5e887fa0c0541ff07
罗凤达 <362861693@qq.com>
2019-05-09 21:55:44 +0800

    新增购物车静态页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from '@/views/GoodsList'
+import CartList from '@/views/CartList'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'GoodsList',
       component: GoodsList
+    },
+    {
+      path: '/cart',
+      name: 'CartList',
+      component: CartList
     }
   ]
 })
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -1,92 +1,104 @@
 <template>
-  <div class="accessory-result-page accessory-page">
-    <nav-header></nav-header>
-    <div class="container">
-      <div class="filter-nav">
-        <span class="sortby">Sort by:</span>
-        <a href="javascript:void(0)" class="default cur">Default</a>
-        <a href="javascript:void(0)" class="price" @click="sortGoods">
-          Price
-          <svg class="icon icon-arrow-short" v-bind:class="{'sort-up':sortFlag}">
-            <use xlink:href="#icon-arrow-short"></use>
-          </svg>
-        </a>
-        <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">选择价位</a>
-      </div>
-      <div class="accessory-result">
-        <!-- filter -->
-        <div class="filter stopPop" id="filter" :class="{'filterby-show':filterBy}">
-          <dl class="filter-price">
-            <dt>价钱:</dt>
-            <dd>
-              <a href="javascript:void(0)" :class="{'cur':priceChecked==='all'}">All</a>
-            </dd>
-            <dd v-for="(item,index) in priceData">
-              <a
-                href="javascript:void(0)"
-                @click="setPriceFilter(index)"
-                :class="{'cur':priceChecked==index}"
-              >{{item.startPrice}} - {{item.endPrice}}</a>
-            </dd>
-          </dl>
+  <div>
+     <nav-header></nav-header>
+      <nav-bread>
+        <span>商品列表</span>
+      </nav-bread>
+    <div class="accessory-result-page accessory-page">
+      <div class="container">
+        <div class="filter-nav">
+          <span class="sortby">Sort by:</span>
+          <a href="javascript:void(0)" class="default cur">Default</a>
+          <a href="javascript:void(0)" class="price" @click="sortGoods">
+            Price
+            <svg class="icon icon-arrow-short" v-bind:class="{'sort-up':sortFlag}">
+              <use xlink:href="#icon-arrow-short"></use>
+            </svg>
+          </a>
+          <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">选择价位</a>
         </div>
+        <div class="accessory-result">
+          <!-- filter -->
+          <div class="filter stopPop" id="filter" :class="{'filterby-show':filterBy}">
+            <dl class="filter-price">
+              <dt>价钱:</dt>
+              <dd>
+                <a href="javascript:void(0)" :class="{'cur':priceChecked==='all'}">All</a>
+              </dd>
+              <dd v-for="(item,index) in priceData">
+                <a
+                  href="javascript:void(0)"
+                  @click="setPriceFilter(index)"
+                  :class="{'cur':priceChecked==index}"
+                >{{item.startPrice}} - {{item.endPrice}}</a>
+              </dd>
+            </dl>
+          </div>
 
-        <!-- search result accessories list -->
-        <div class="accessory-list-wrap">
-          <div class="accessory-list col-4">
-            <ul>
-              <li v-for="(item, index) in goodsData">
-                <div class="pic">
-                  <a href="#">
-                    <img v-lazy="'/static/'+item.productImage" alt width="150px" height="100px">
-                  </a>
-                </div>
-                <div class="main">
-                  <div class="name">{{item.productName}}</div>
-                  <div class="price">{{item.salePrice}}</div>
-                  <div class="btn-area">
-                    <a href="javascript:;" class="btn btn--m" @click="addCart(item.productId)">加入购物车</a>
+          <!-- search result accessories list -->
+          <div class="accessory-list-wrap">
+            <div class="accessory-list col-4">
+              <ul>
+                <li v-for="(item, index) in goodsData">
+                  <div class="pic">
+                    <a href="#">
+                      <img v-lazy="'/static/'+item.productImage" alt width="150px" height="100px">
+                    </a>
                   </div>
+                  <div class="main">
+                    <div class="name">{{item.productName}}</div>
+                    <div class="price">{{item.salePrice}}</div>
+                    <div class="btn-area">
+                      <a
+                        href="javascript:;"
+                        class="btn btn--m"
+                        @click="addCart(item.productId)"
+                      >加入购物车</a>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+              <div
+                v-infinite-scroll="loadMore"
+                infinite-scroll-disabled="busy"
+                infinite-scroll-distance="10"
+              >
+                <div class="lds-ripple" v-show="loading">
+                  <div></div>
+                  <div></div>
                 </div>
-              </li>
-            </ul>
-            <div
-              v-infinite-scroll="loadMore"
-              infinite-scroll-disabled="busy"
-              infinite-scroll-distance="10"
-            >
-              <div class="lds-ripple" v-show="loading">
-                <div></div>
-                <div></div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
+      <Modal v-bind:mdShow="mdShow" v-on:close="closeModal">
+        <p slot="message">请先登录，否则无法添加到购物车中。</p>
+        <div slot="btnGroup">
+          <a href="javascript:;" class="btn btn-m" @click="mdShow=false">关闭</a>
+        </div>
+      </Modal>
+      <Modal v-bind:mdShow="mdShowCart" v-on:close="closeModal">
+        <p slot="message">
+          <svg class="icon-status-ok">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-cart"></use>
+          </svg>
+          <span>加入购物成功！</span>
+        </p>
+        <div slot="btnGroup">
+          <a href="javascript:;" class="btn btn-m" @click="mdShowCart=false">继续购物</a>
+          <router-link href="javascript:;" class="btn btn-m" to="/cart">查看购物车</router-link>
+        </div>
+      </Modal>
+      <nav-footer></nav-footer>
     </div>
-    <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
-    <Modal v-bind:mdShow="mdShow" v-on:close="closeModal">
-      <p slot="message">请先登录，否则无法添加到购物车中。</p>
-      <div slot="btnGroup">
-        <a href="javascript:;" class="btn btn-m" @click="mdShow=false">关闭</a>
-      </div>
-    </Modal>
-    <Modal v-bind:mdShow="mdShowCart" v-on:close="closeModal">
-      <p slot="message">
-        <svg class="icon-status-ok">
-          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-cart"></use>
-        </svg>
-        <span>加入购物成功！</span>
-      </p>
-      <div slot="btnGroup">
-        <a href="javascript:;" class="btn btn-m" @click="mdShowCart=false">继续购物</a>
-        <router-link href="javascript:;" class="btn btn-m" to="/cart">查看购物车</router-link>
-      </div>
-    </Modal>
   </div>
 </template>
 <script >
 import NavHeader from "./../components/NavHeader";
+import NavFooter from "./../components/NavFooter";
+import NavBread from "./../components/NavBread";
 import axios from "axios";
 import Modal from "./../components/Modal";
 export default {
@@ -132,6 +144,8 @@ export default {
   },
   components: {
     NavHeader,
+    NavFooter,
+    NavBread,
     Modal
   },
   methods: {
