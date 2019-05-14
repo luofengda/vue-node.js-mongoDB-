be27345edee85024f7e1900531127e876979a67f
罗凤达 <362861693@qq.com>
2019-05-14 20:55:38 +0800

    新增提交订单页面

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import GoodsList from '@/views/GoodsList'
 import CartList from '@/views/CartList'
 import Address from '@/views/Address'
+import OrderConfirm from '@/views/OrderConfirm'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/address',
       name: 'Address',
       component: Address
+    },
+    {
+      path: '/orderConfirm',
+      name: 'orderConfirm',
+      component: OrderConfirm
     }
   ]
 })
